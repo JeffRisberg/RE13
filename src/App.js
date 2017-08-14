72cb8634caff45df4c52b543acd44dcf007d9c96
JeffRisberg <jrisberg@aol.com>
2017-08-14 10:10:28 +0100

    finished lesson 3

--- src/App.js
+++ src/App.js
@@ -4,33 +4,31 @@ import './App.css';
 
 class App extends Component {
     render() {
-	return (
-      <div className="App">
-        <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React with Redux</h2>
-        </div>
-        <div className="Todo-App">
-          <form>
-            <input type="text"/>
-          </form>
-          <div className="Todo-List">
-            <ul>
-              <li>
-                <input type="checkbox" /> Create Static UI
-              </li>
-              <li>
-                <input type="checkbox" /> Create Initial State
-              </li>
-              <li>
-                <input type="checkbox" /> Use State to Render UI
-              </li>
-            </ul>
-          </div>
+        return (
+            <div className="App">
+              <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>Welcome to React with Redux</h2>
+              </div>
+              <div className="Todo-App">
+                <form>
+                  <input type="text"/>
+                </form>
+                <div className="Todo-List">
+                  <ul>
+                      {this.props.todos.map(todo => (
+                          <li key={todo.id}>
+                            <input type="checkbox" defaultChecked={todo.isComplete} />
+                              {todo.name}
+                          </li>
+                      ))}
 
-        </div>
-      </div>
-		);
+                  </ul>
+                </div>
+
+              </div>
+            </div>
+        );
     }
 }
 
