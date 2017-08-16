89061fb9a359af8b0938583077fbab7a108c853e
JeffRisberg <jrisberg@aol.com>
2017-08-16 03:52:23 +0100

    finished lesson 4

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-              <div className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>Welcome to React with Redux</h2>
-              </div>
-              <div className="Todo-App">
-                <form>
-                  <input type="text"/>
-                </form>
-                <div className="Todo-List">
-                  <ul>
-                      {this.props.todos.map(todo => (
-                          <li key={todo.id}>
-                            <input type="checkbox" defaultChecked={todo.isComplete} />
-                              {todo.name}
-                          </li>
-                      ))}
-
-                  </ul>
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React with Redux</h2>
                 </div>
+                <div className="Todo-App">
+                    <TodoForm />
+                    <TodoList todos={this.props.todos} />
 
-              </div>
+                </div>
             </div>
         );
     }
