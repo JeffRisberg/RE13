45e5b54a5d0a38703ac6f5cb18010ca5aefac8e7
JeffRisberg <jrisberg@aol.com>
2017-08-18 01:36:33 +0100

    finished lesson 21

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,20 +1,21 @@
-import { getTodos, createTodo } from '../lib/todoServices';
-import { showMessage } from './messages';
+import {getTodos, createTodo} from '../lib/todoServices'
+import {showMessage} from './messages'
 
 const initState = {
     todos: [],
     currentTodo: ''
 }
 
-const TODO_ADD = 'TODO_ADD'
-const TODOS_LOAD = 'TODOS_LOAD'
+export const TODO_ADD = 'TODO_ADD'
+export const TODOS_LOAD = 'TODOS_LOAD'
 const CURRENT_UPDATE = 'CURRENT_UPDATE'
 
-export const updateCurrent = (val) => ({ type: CURRENT_UPDATE, payload: val })
-export const loadTodos = (todos) => ({ type: TODOS_LOAD, payload: todos })
-export const addTodo = (todo) => ({ type: TODO_ADD, payload: todo })
+export const updateCurrent = (val) => ({type:CURRENT_UPDATE, payload: val})
+export const loadTodos = (todos) => ({type: TODOS_LOAD, payload: todos})
+export const addTodo = (todo) => ({type: TODO_ADD, payload: todo})
 export const fetchTodos = () => {
     return (dispatch) => {
+        dispatch(showMessage('Loading Todos'))
         getTodos()
             .then(todos => dispatch(loadTodos(todos)))
     }
@@ -24,21 +25,18 @@ export const saveTodo = (name) => {
     return (dispatch) => {
         dispatch(showMessage('Saving Todo'))
         createTodo(name)
-            .then(res => {
-                dispatch(addTodo(res));
-                dispatch(showMessage(''));
-            })
+            .then(res => dispatch(addTodo(res)))
     }
 }
 
 export default (state = initState, action) => {
     switch (action.type) {
         case TODO_ADD:
-            return { ...state, currentTodo: '', todos: state.todos.concat(action.payload) }
+            return {...state, currentTodo: '', todos: state.todos.concat(action.payload)}
         case TODOS_LOAD:
-            return { ...state, todos: action.payload }
+            return {...state, todos: action.payload}
         case CURRENT_UPDATE:
-            return { ...state, currentTodo: action.payload }
+            return {...state, currentTodo: action.payload}
         default:
             return state
     }
